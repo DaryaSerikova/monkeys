8434e1ce2646f1020d895ef7f649acd421fedfdb
Daria Serikova <dvserikova@gmail.com>
2025-06-25 17:33:18 +0700

    cart: prepared data for backend, function check phone

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -1,9 +1,10 @@
+'use client';
 import React from 'react';
 import s from './cart.module.scss';
 import { useAppSelector } from '@/lib/hooks';
 import { ICardItem } from '@/shared/types/ICartItem';
 import { InputMask } from '@react-input/mask';
-
+import { useState } from 'react';
 import Button from '../button/button';
 
 
@@ -13,6 +14,51 @@ const Cart = (props: Props) => {
 
   const cart = useAppSelector((state) => state.cartReducer);
   console.log("cart: ", cart);
+  const [phone, setPhone] = useState('');
+  const [error, setError] = useState('');
+
+  const getItemsBackend = (arr: ICardItem[]) => arr?.map(
+    (cartItem: ICardItem) => ({id: cartItem?.id, amount: cartItem?.amount}));
+  
+
+  const getInfoForBackend = () => {
+    const cartArray = getItemsBackend(cart?.items);
+    const resObj = {
+      phone: phone.replace(/[^\d]/g, ""),
+      cart: cartArray,
+    }
+    const result = JSON.stringify(resObj);
+    console.log(result)
+    return result;
+  }
+
+  const checkPhone = (phoneNumber: string) => {
+    console.log("phone ", phone)
+
+    if (phoneNumber.length !== 18) {
+      if (phoneNumber === '') {
+        console.log('Введите номер телефона!')
+        setError('Введите номер телефона!');
+        return false;
+      } else {
+        console.log('Номер введен не полностью!')
+        setError('Номер введен не полностью!')
+        return false;
+      }
+    }
+    setError('');
+    return true;
+  }
+
+  const handleOrder = () => {
+    const isAccess: boolean = checkPhone(phone);
+    if (isAccess) {
+      getInfoForBackend();
+      console.log('ЗАКАЗАНО!')
+    } else {
+      console.log('НЕ ЗАКАЗАНО')
+    }
+  }
 
 
   return (
@@ -32,16 +78,16 @@ const Cart = (props: Props) => {
 
         <footer>
           <InputMask
-            mask="+7 (___) ___-__-__"
+            mask="+7 (___) ___ __-__"
             replacement={{ _: /\d/ }} // только цифры
-            // value={phone}
-            // onChange={handleChange}
-            placeholder="+7 (999) 999-99-99"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            placeholder="+7 (999) 999 99-99"
             className={s.phone}
 
           />
 
-          <Button onclick={() => console.log('ЗАКАЗАНО!')}>заказать</Button>
+          <Button onclick={handleOrder}>заказать</Button>
         </footer>
       </div>
     </article>
